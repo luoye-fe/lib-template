f78371401b5641dd17dd22379b5643acaf1bd7c6
luoye <842891024@qq.com>
2016-11-01 15:30:47 +0800

    update config.js

--- template/build/config.js
+++ template/build/config.js
@@ -6,15 +6,16 @@ var env = require('./env.js');
 
 var config = {
 	entry: path.join(__dirname, '../src/app.js'),
-	moduleName: '{{ moduleName }}',
-	format: 'umd',
 	plugins: [
 		babel()
 	]
 };
 
+
 if (env === 'dev') {
 	module.exports = Object.assign({
+		format: 'umd',
+		moduleName: '{{ moduleName }}'
 		dest: path.join(__dirname, '../dist/{{ name }}.js')
 	}, config);
 } else {
