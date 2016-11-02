87b92579c420c547ca5a2626a18e9336111dbd29
luoye <842891024@qq.com>
2016-11-02 16:26:05 +0800

    add eslint

--- template/build/config.js
+++ template/build/config.js
@@ -1,16 +1,17 @@
 var path = require('path');
 
 var babel = require('rollup-plugin-babel');
+var eslint = require('rollup-plugin-eslint');
 
 var env = require('./env.js');
 
 var config = {
 	entry: path.join(__dirname, '../src/app.js'),
 	plugins: [
+		eslint(),
 		babel()
 	]
 };
-
 
 if (env === 'dev') {
 	module.exports = Object.assign({
